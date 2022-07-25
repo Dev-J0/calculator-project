42ffa5c9df397b9e4ddca354184e6774b931591f
d0bsidian <101651861+d0bsidian@users.noreply.github.com>
2022-07-25 13:53:32 +0200

    starting

---
+++ app.js
@@ -0,0 +1,42 @@
+function add(a,b) {
+    return a + b;
+}
+
+
+function substract(a,b) {
+    return a - b;
+}
+
+
+function multiply(a,b) {
+    return a * b;
+}
+
+
+function divide(a,b) {
+    return a / b;
+}
+
+
+function operate(operator, a,b) {
+    a = Number(a)
+    b = Number(b)
+
+    switch(operator) {
+        case '+':
+            return add(a,b)
+
+        case '-':
+            return substract(a,b)
+
+        case '*':
+            return multiply(a,b)
+
+        case '/':
+            if(b === 0) return 0
+            else return divide(a,b)
+        
+         default:
+            return null
+    }
+}
